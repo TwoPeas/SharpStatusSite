72ae48897d8a8a6b9a2f8f210905186705c67d64
Ben Hyrman <ben.hyrman@gmail.com>
2020-07-12 13:50:45 -0400

    Trying out a call

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,9 +1,25 @@
+const http = require('https');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    context.res = {
-        body: {
-          text: "a sample key: " + process.env["ButtonDownToken"]
+    const options = {
+        hostname: 'api.buttondown.email',
+        port: 443,
+        path: '/v1/subscribers',
+        method: 'GET',
+        headers: {
+            'Authorization': 'token ' + process.env["ButtonDownToken"]
         }
+    }
+
+    let request = http.request(options, (res) => {
+        res.on('data', (d) => {
+            response += d;
+        })
+    })
+
+    context.res = {
+        body: response
     };
 };
