cd417791e2b9148ab192bbd8495bda3858bfeac3
Ben Hyrman <ben.hyrman@gmail.com>
2020-07-12 14:32:12 -0400

    let's try a post?

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -3,33 +3,43 @@ const https = require('https');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    const data = JSON.stringify({
+        "email": "ben.hyrman+test1@gmail.com",
+        "referrer_url": "sharpstatus.com",
+        "metadata": {},
+        "tags": [
+            "beta subscriber"
+        ]
+    })
+
     const options = {
         hostname: 'api.buttondown.email',
         port: 443,
         path: '/v1/subscribers',
-        method: 'GET',
+        method: 'POST',
         headers: {
-            'Authorization': 'token ' + process.env["ButtonDownToken"]
+            'Authorization': 'token ' + process.env["ButtonDownToken"],
+            'Content-Type': 'application/json',
+            'Content-Length': data.length        
         }
     }
+
+    let data = ''
     
     const request = https.request(options, (res) => {
-        let data = ''
          res.on('data', (chunk) => {
            data += chunk.toString();
          });
-         res.on('end', () => {
-            context.res = {
-                body: data
-            };
-         })
        });
        
     request.on('error', (e) => {
-        context.res = {
-            body: 'error'
-        }
+        data = 'error'
     });
 
+    request.write(data)
     request.end()
+
+    context.res = {
+        body: data
+    };
 };
